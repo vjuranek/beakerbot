e9dd6be37d80e239e477d54be2ccc48a799bcf0d
Vojtech Juranek <vjuranek@redhat.com>
2013-03-20 00:04:45 +0100

    Removed unneeded code

--- lib/beakerbot/ssh_handler.rb
+++ lib/beakerbot/ssh_handler.rb
@@ -1,12 +1,10 @@
+require 'net/ssh'
+
 module BeakerBot
   
   class SshHandler < MessageHandler
     register(:ssh, /^ssh/)
 
-    def initialize
-      puts "SSH created!"
-    end
-    
     def self.handle message
       return "Ssh handler, handling message #{message}"
     end
