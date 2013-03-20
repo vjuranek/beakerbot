d60087fbd72946590b16cbeba1809c662af703ea
Vojtech Juranek <vjuranek@redhat.com>
2013-03-20 02:59:41 +0100

    Add possibility to execute command over ssh

--- lib/beakerbot/ssh_handler.rb
+++ lib/beakerbot/ssh_handler.rb
@@ -6,11 +6,35 @@ module BeakerBot
     register(:ssh, /^ssh/)
 
     def handle message
-      return "Ssh handler, handling message #{message}"
+      if not /^ssh exec\s(\w+)\son\s(.*)\sas\s(\w+)$/ =~ message
+        return "#{message} doesn't seem to be valid request, syntax is #{self.class.get_syntax}"
+      end
+
+      command = $1
+      hostname = $2
+      username = $3
+      reply = "Executing '#{command}' on host '#{hostname}' as a user '#{username}' ... \n"
+
+      begin
+        ssh = Net::SSH.start(hostname, username)
+        reply += ssh.exec!(command)
+        ssh.close
+      rescue Exception => err
+        reply += "Somethig went wrong: #{err}"
+      end
+
+      return reply
+    end
+
+    def exec_cmd(cmd)
+    end
+
+    def self.get_syntax
+      return "ssh exec [ssh commnd] on [host] as [user]"
     end
 
     def self.get_help
-      return "Run 'ssh exec [ssh commnd] on [host]' to execute ssh command on given host"
+      return "Run '#{self.get_syntax}' to execute ssh command on given host"
     end
    
   end
