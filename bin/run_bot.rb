f8deb3f7b2852fa55a1bf54176adbfd93a7f4beb
Vojtech Juranek <vjuranek@redhat.com>
2013-03-20 02:57:37 +0100

    Remove unused code

--- bin/run_bot.rb
+++ bin/run_bot.rb
@@ -4,10 +4,5 @@ $LOAD_PATH.push File.dirname(__FILE__) + "/../lib"
 
 require 'beakerbot'
 
-# mh = BeakerBot::MessageHandler.new
-# puts mh.handle_message "ssh test"
-# puts mh.handle_message "beaker test"
-# puts mh.handle_message "sdsds"
-
 bot = BeakerBot::BeakerBot.new 
 bot.start
