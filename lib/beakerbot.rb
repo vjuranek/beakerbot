fa9c22f71c164cf7f3192a115a63c2058f9356d6
Vojtech Juranek <vjuranek@redhat.com>
2013-03-20 02:59:11 +0100

    Fix problem with require order

--- lib/beakerbot.rb
+++ lib/beakerbot.rb
@@ -1,3 +1,9 @@
-Dir.glob(File.dirname(__FILE__) + "/beakerbot/*.rb") do |file|
-  require file
-end
+# Dir.glob(File.dirname(__FILE__) + "/beakerbot/*.rb") do |file|
+#   require file
+# end
+
+require "beakerbot/beaker_bot.rb"
+require "beakerbot/message_handler.rb"
+require "beakerbot/beaker_handler.rb"
+require "beakerbot/help_handler.rb"
+require "beakerbot/ssh_handler.rb"
